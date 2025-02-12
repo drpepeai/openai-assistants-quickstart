482b269afa1609b7865d08610e769d9f6439f4d1
Victor Maragioglio <maragiogliov@protonmail.com>
2025-02-12 15:31:14 +0100

    img remove still testing

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,9 @@ export default function Home() {
       {userId ?
         <Chat /> :
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <img src="/banner-no-bg.png" alt="banner" className="mx-auto w-1/2 h-1/2" />
           <div className="text-center">
             <h1 className="text-balance text-3xl font-semibold tracking-tight text-white mt-4">
-              Hello I am DrPepe.ai (Demo)
+              Hello I am DrPepe.ai 
             </h1>
             <p className="mt-2 text-pretty text-2xl font-medium text-zinc-500">
               Tell me how I can help you live forever
