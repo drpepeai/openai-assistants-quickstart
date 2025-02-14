be6024a0a6d07e8881ba4885032c53af9b87d958
Victor Maragioglio <maragiogliov@protonmail.com>
2025-02-14 01:21:09 +0100

    navbar

--- app/components/SideBarContainer.tsx
+++ app/components/SideBarContainer.tsx
@@ -39,16 +39,16 @@ export default function SideBarContainer({ mobile }) {
         {userId && (
           <button
             onClick={handleLogout}
-            className="p-2 bg-red-600 text-white rounded-lg flex items-center gap-2"
+            className="p-2 bg-grey-800 text-white rounded-lg flex justify-center items-center gap-2"
           >
-            <ArrowRightCircleIcon className="h-5 w-5" /> 
+            <ArrowRightCircleIcon className="h-6 w-6" /> 
           </button>
         )}
       </div>
 
       {/* Sidebar */}
       {isOpen && (
-        <div className="w-64 h-full pt-[7rem] fixed left-0 top-0 bg-[#181818] text-[#D1D1D1] px-[1.8rem] text-sm font-primary flex flex-col justify-start transition-transform duration-300">
+        <div className="w-64 h-full pt-[7rem] fixed left-0 top-0 bg-[#181818] text-[#D1D1D1] px-[1.8rem] pr-0 text-sm font-primary flex flex-col justify-start transition-transform duration-300">
           <div className="flex h-[30rem] overflow-scroll flex-col mb-4">
             <div className="text-[#5BB8F0] text[14px] font-primary">Threads</div>
             {threadIds.map((threadId) => (
