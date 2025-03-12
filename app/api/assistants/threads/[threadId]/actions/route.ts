8830db61f2a49527db3e3c9d41ff2b7009c5cbe0
Victor Maragioglio <maragiogliov@protonmail.com>
2025-03-12 17:24:18 +0100

    openai api request comment  out

--- app/api/assistants/threads/[threadId]/actions/route.ts
+++ app/api/assistants/threads/[threadId]/actions/route.ts
@@ -1,3 +1,23 @@
+/*
+  import { openai } from "@/app/openai";
+
+// Send a new message to a thread
+export async function POST(request, { params: { threadId } }) {
+  const { toolCallOutputs, runId } = await request.json();
+
+  const stream = openai.beta.threads.runs.submitToolOutputsStream(
+    threadId,
+    runId,
+    { tool_outputs: toolCallOutputs }
+  );
+
+  return new Response(stream.toReadableStream());
+}
+  
+  */
+
+
+
 export async function POST(request: Request) {
   try {
     const { toolCallOutputs, runId, threadId } = await request.json();
